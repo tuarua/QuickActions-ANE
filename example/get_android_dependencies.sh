882fd52aebc2c9a7991b19b77fd0b78308cfcf9f
Eoin Landy <info@tuarua.co.uk>
2019-08-02 00:25:47 +0100

    1.3.0

--- example/get_android_dependencies.sh
+++ example/get_android_dependencies.sh
@@ -5,5 +5,5 @@ FreKotlinVersion="1.8.0"
 SupportV4Version="27.1.0"
 
 wget -O android_dependencies/com.tuarua.frekotlin-$FreKotlinVersion.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
-wget -O android_dependencies/com.android.support.support-v4-$SupportV4Version.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.support-v4-$SupportV4Version-air33.ane?raw=true
+wget -O android_dependencies/com.android.support.support-v4-$SupportV4Version.ane https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.support-v4-$SupportV4Version.ane?raw=true
 wget -O ../native_extension/ane/QuickActionsANE.ane https://github.com/tuarua/QuickActions-ANE/releases/download/$AneVersion/QuickActionsANE.ane?raw=true
